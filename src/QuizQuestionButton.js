18865d0bb87acfe351759356372dfa5afbb01807
naveenkumarpsnk <42308898+naveenkumarpsnk@users.noreply.github.com>
2022-05-30 11:21:43 +0000

    commit-nav

---
+++ src/QuizQuestionButton.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+class QuizQuestionButton extends Component
+{
+    render()
+    {
+        return(
+            <li>
+                <button></button>
+            </li>
+        )
+    }
+
+}
+export default QuizQuestionButton
