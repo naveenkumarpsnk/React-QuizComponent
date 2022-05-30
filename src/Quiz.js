7f9d08032804d60da221195ff0750c794f7b2ef1
naveenkumarpsnk <42308898+naveenkumarpsnk@users.noreply.github.com>
2022-05-30 11:43:45 +0000

    commit-nav

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,10 +10,13 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 }
   }
   render() {
+      const isQuizEnd=true
     return (
       <div>
-          <QuizEnd />
+
+         {isQuizEnd ? <QuizEnd />:
         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+    }
       </div>
     )
   }
